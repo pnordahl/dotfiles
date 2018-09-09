853f688b314e9b47f7769be3cc1bb1839d97f2c5
Patrick Nordahl <plnordahl@gmail.com>
2018-09-09 18:50:58 -0500

    update link script

--- link-all.sh
+++ link-all.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-ln -s .vimrc ~/.vimrc
-ln -s .vim ~/.vim
+PWD=$(pwd)
+
+ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.vim ~/.vim
+
+ln -s $PWD/.zshrc ~/.zshrc
+ln -s $PWD/.tmux ~/.tmux
+ln -s $PWD/.tmux.conf ~/.tmux.conf
